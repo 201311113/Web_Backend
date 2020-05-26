a596645de84c296b8b0ac56b0bc4e36eb9bb1e98
mooss <antjdlekd123@naver.com>
2020-05-26 17:54:47 +0900

    JSP 20강 실습 커넥션풀을 사용하여 DB접근하는 법 실습

---
+++ JSP실습/JSP_20_3_exfile_Cpull/src/com/javalec/daoto/MemberDAO.java
@@ -0,0 +1,59 @@
+package com.javalec.daoto;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import javax.naming.*;
+import javax.sql.DataSource;
+
+
+public class MemberDAO {
+	
+	private DataSource dataSource;
+	public MemberDAO() {
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource)context.lookup("java:comp/env/jdbc/Oracle18");
+		}catch(Exception e) {e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<MemberDTO> memberSelect(){
+		
+		ArrayList<MemberDTO> dtos = new ArrayList<MemberDTO>();
+		
+		Connection con = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = dataSource.getConnection();
+			stmt = con.createStatement();
+			rs = stmt.executeQuery("select * from member");
+			
+			while(rs.next()) {
+				String name = rs.getString("name");
+				String id = rs.getString("id");
+				String pw = rs.getString("pw");
+				String phone1 = rs.getString("phone1");
+				String phone2 = rs.getString("phone2");
+				String phone3 = rs.getString("phone3");
+				String gender = rs.getString("gender");
+				
+				MemberDTO dto = new MemberDTO(name, id,pw,phone1,phone2,phone3,gender);
+				dtos.add(dto);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return dtos;
+	}
+}

9aa483c2e305e351ad0aff2d2969594b07a0b486
mooss <antjdlekd123@naver.com>
2020-07-13 23:04:32 +0900

    Spring 실습 MVC패턴으로 설계 후 JDBC를 이용한 게시판 만들기

---
+++ Spring실습/Spring_MVC_Board/src/main/java/com/javalec/command/BCommand.java
@@ -0,0 +1,8 @@
+package com.javalec.command;
+
+import org.springframework.ui.Model;
+
+public interface BCommand {
+	
+	void execute(Model model) ;
+}
---
+++ Spring실습/Spring_MVC_Board/src/main/java/com/javalec/command/BContentCommand.java
@@ -0,0 +1,24 @@
+package com.javalec.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
+import com.javalec.dao.BDao;
+import com.javalec.dto.BDto;
+
+public class BContentCommand implements BCommand{
+	@Override
+	public void execute(Model model) {
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest request = (HttpServletRequest)map.get("request");
+		String bId = request.getParameter("bId");
+		
+		BDao dao = new BDao();
+		BDto dto = dao.content_view(bId);
+		
+		model.addAttribute("content_view",dto);
+	}
+}
